f66190f4096346ff3cdca735caf1e49d38918a94
NithyanathanA <nithy.b.tech.it@gmail.com>
2024-05-01 11:12:01 +0530

    Spring-boot

---
+++ src/main/java/com/wep/Security/database/RegisterAndLogoutApplication.java
@@ -0,0 +1,13 @@
+package com.wep.Security.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAndLogoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAndLogoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wep/Security/database/RegisterAndLogoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wep.Security.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterAndLogoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
